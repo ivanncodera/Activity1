2b08a1bb455579e5fb5d608580fe89361c7dbc03
Roselyn C. Mongado <roselynmongado@gmail.com>
2025-08-06 13:03:14 +0800

    CV

---
+++ CV_Rose/CV/OvalPictureBox.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace CV
+{
+    public class OvalPictureBox : PictureBox
+    {
+        public OvalPictureBox()
+        {
+            this.BackColor = Color.DarkGray;
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            using (var gp = new GraphicsPath())
+            {
+                gp.AddEllipse(new Rectangle(0, 0, this.Width - 1, this.Height - 1));
+                this.Region = new Region(gp);
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            using (GraphicsPath gp = new GraphicsPath())
+            {
+                gp.AddEllipse(0, 0, this.Width - 1, this.Height - 1);
+                this.Region = new Region(gp);
+                e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                e.Graphics.DrawEllipse(new Pen(new SolidBrush(this.BackColor), 1), 0, 0, this.Width - 1, this.Height - 1);
+            }
+        }
+    }
+}
