ee641f826f94acb577a70f19829550a8df16c500
ivanncodera <codera.ivannbsis2023@gmail.com>
2025-08-06 12:23:36 +0800

    gilles

--- WinFormsApp1/Gilles.Designer.cs
+++ WinFormsApp1/Gilles.Designer.cs
@@ -6,7 +6,36 @@
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
-
+        
+        // Field declarations
+        private Label label5;
+        private Button button1;
+        private PictureBox pictureBox1;
+        private Button button2;
+        private Label label1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label2;
+        private Button button3;
+        private RichTextBox richTextBox2;
+        private Button button4;
+        private RichTextBox richTextBox3;
+        private Button button5;
+        private RichTextBox richTextBox4;
+        private Button button6;
+        private RichTextBox richTextBox5;
+        private Label label3;
+        private TextBox textBox1;
+        private Label label4;
+        private TextBox textBox4;
+        private Label label6;
+        private TextBox textBox5;
+        private Label label7;
+        private RichTextBox richTextBox6;
+        private Button button7;
+        private Button button9;
+        private VScrollBar vScrollBar1;
+        
         /// <summary>
         /// Clean up any resources being used.
         /// </summary>
@@ -28,12 +57,381 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Gilles";
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            button2 = new Button();
+            label1 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label2 = new Label();
+            button3 = new Button();
+            richTextBox2 = new RichTextBox();
+            button4 = new Button();
+            richTextBox3 = new RichTextBox();
+            button5 = new Button();
+            richTextBox4 = new RichTextBox();
+            button6 = new Button();
+            richTextBox5 = new RichTextBox();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            textBox4 = new TextBox();
+            label6 = new Label();
+            textBox5 = new TextBox();
+            label7 = new Label();
+            richTextBox6 = new RichTextBox();
+            button7 = new Button();
+            button9 = new Button();
+            vScrollBar1 = new VScrollBar();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaptionText;
+            button1.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(-7, 0);
+            button1.Margin = new Padding(0, 6, 0, 6);
+            button1.Name = "button1";
+            button1.Size = new Size(1183, 57);
+            button1.TabIndex = 0;
+            button1.Text = "CURRICULUM VITAE";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.ButtonFace;
+            pictureBox1.Location = new Point(90, 89);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(160, 152);
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.LightCoral;
+            button2.Font = new Font("Tahoma", 7.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = SystemColors.ActiveCaptionText;
+            button2.Location = new Point(90, 252);
+            button2.Name = "button2";
+            button2.Size = new Size(160, 29);
+            button2.TabIndex = 4;
+            button2.Text = "UPLOAD PHOTO";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ActiveCaptionText;
+            label1.Location = new Point(367, 127);
+            label1.Name = "label1";
+            label1.Size = new Size(121, 21);
+            label1.TabIndex = 5;
+            label1.Text = "FULL NAME:";
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Arial", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(494, 110);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(616, 45);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Arial", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox3.Location = new Point(494, 161);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(204, 45);
+            textBox3.TabIndex = 7;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.ActiveCaptionText;
+            label2.Location = new Point(378, 178);
+            label2.Name = "label2";
+            label2.Size = new Size(87, 21);
+            label2.TabIndex = 8;
+            label2.Text = "STATUS:";
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ButtonHighlight;
+            button3.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.ForeColor = SystemColors.ActiveCaptionText;
+            button3.Location = new Point(352, 229);
+            button3.Margin = new Padding(0, 6, 0, 6);
+            button3.Name = "button3";
+            button3.Size = new Size(911, 37);
+            button3.TabIndex = 9;
+            button3.Text = "ABOUT ME";
+            button3.TextAlign = ContentAlignment.MiddleLeft;
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // richTextBox2
+            // 
+            richTextBox2.Font = new Font("Arial", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            richTextBox2.Location = new Point(370, 278);
+            richTextBox2.Margin = new Padding(0, 6, 0, 6);
+            richTextBox2.Name = "richTextBox2";
+            richTextBox2.Size = new Size(771, 101);
+            richTextBox2.TabIndex = 10;
+            richTextBox2.Text = "";
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.ButtonHighlight;
+            button4.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.ForeColor = SystemColors.ActiveCaptionText;
+            button4.Location = new Point(351, 411);
+            button4.Margin = new Padding(0, 6, 0, 6);
+            button4.Name = "button4";
+            button4.Size = new Size(911, 37);
+            button4.TabIndex = 11;
+            button4.Text = "EDUCATION";
+            button4.TextAlign = ContentAlignment.MiddleLeft;
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // richTextBox3
+            // 
+            richTextBox3.Font = new Font("Arial", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            richTextBox3.Location = new Point(367, 466);
+            richTextBox3.Margin = new Padding(0, 6, 0, 6);
+            richTextBox3.Name = "richTextBox3";
+            richTextBox3.Size = new Size(771, 141);
+            richTextBox3.TabIndex = 12;
+            richTextBox3.Text = "";
+            // 
+            // button5
+            // 
+            button5.BackColor = SystemColors.ButtonHighlight;
+            button5.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button5.ForeColor = SystemColors.ActiveCaptionText;
+            button5.Location = new Point(351, 641);
+            button5.Margin = new Padding(0, 6, 0, 6);
+            button5.Name = "button5";
+            button5.Size = new Size(911, 37);
+            button5.TabIndex = 13;
+            button5.Text = "EXPERIENCE";
+            button5.TextAlign = ContentAlignment.MiddleLeft;
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // richTextBox4
+            // 
+            richTextBox4.Font = new Font("Arial", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            richTextBox4.Location = new Point(370, 694);
+            richTextBox4.Margin = new Padding(0, 6, 0, 6);
+            richTextBox4.Name = "richTextBox4";
+            richTextBox4.Size = new Size(771, 141);
+            richTextBox4.TabIndex = 14;
+            richTextBox4.Text = "";
+            // 
+            // button6
+            // 
+            button6.BackColor = SystemColors.ButtonHighlight;
+            button6.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button6.ForeColor = SystemColors.ActiveCaptionText;
+            button6.Location = new Point(351, 857);
+            button6.Margin = new Padding(0, 6, 0, 6);
+            button6.Name = "button6";
+            button6.Size = new Size(911, 37);
+            button6.TabIndex = 15;
+            button6.Text = "REFERENCES";
+            button6.TextAlign = ContentAlignment.MiddleLeft;
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // richTextBox5
+            // 
+            richTextBox5.Font = new Font("Arial", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            richTextBox5.Location = new Point(367, 906);
+            richTextBox5.Margin = new Padding(0, 6, 0, 6);
+            richTextBox5.Name = "richTextBox5";
+            richTextBox5.Size = new Size(771, 98);
+            richTextBox5.TabIndex = 16;
+            richTextBox5.Text = "";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = SystemColors.GradientActiveCaption;
+            label3.Font = new Font("Arial", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = SystemColors.ActiveCaptionText;
+            label3.Location = new Point(43, 319);
+            label3.Name = "label3";
+            label3.Size = new Size(97, 21);
+            label3.TabIndex = 17;
+            label3.Text = "CONTACT";
+            label3.Click += label3_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Arial", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(138, 357);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(204, 27);
+            textBox1.TabIndex = 18;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = SystemColors.GradientActiveCaption;
+            label4.Font = new Font("Arial", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.ActiveCaptionText;
+            label4.Location = new Point(2, 362);
+            label4.Name = "label4";
+            label4.Size = new Size(132, 18);
+            label4.TabIndex = 19;
+            label4.Text = "CELLPHONE NO.";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = SystemColors.GradientActiveCaption;
+            label5.Font = new Font("Arial", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = SystemColors.ActiveCaptionText;
+            label5.Location = new Point(30, 399);
+            label5.Name = "label5";
+            label5.Size = new Size(57, 18);
+            label5.TabIndex = 20;
+            label5.Text = "EMAIL:";
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Arial", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox4.Location = new Point(94, 395);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(248, 27);
+            textBox4.TabIndex = 21;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = SystemColors.GradientActiveCaption;
+            label6.Font = new Font("Arial", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = SystemColors.ActiveCaptionText;
+            label6.Location = new Point(5, 436);
+            label6.Name = "label6";
+            label6.Size = new Size(84, 18);
+            label6.TabIndex = 22;
+            label6.Text = "ADDRESS:";
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Arial", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox5.Location = new Point(90, 430);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(248, 27);
+            textBox5.TabIndex = 23;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = SystemColors.GradientActiveCaption;
+            label7.Font = new Font("Arial", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = SystemColors.ActiveCaptionText;
+            label7.Location = new Point(43, 535);
+            label7.Name = "label7";
+            label7.Size = new Size(74, 21);
+            label7.TabIndex = 24;
+            label7.Text = "SKILLS";
+            label7.Click += label7_Click;
+            // 
+            // richTextBox6
+            // 
+            richTextBox6.Font = new Font("Arial", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            richTextBox6.ForeColor = SystemColors.Desktop;
+            richTextBox6.Location = new Point(74, 562);
+            richTextBox6.Margin = new Padding(0, 6, 0, 6);
+            richTextBox6.Name = "richTextBox6";
+            richTextBox6.Size = new Size(248, 273);
+            richTextBox6.TabIndex = 25;
+            richTextBox6.Text = "";
+            // 
+            // button7
+            // 
+            button7.BackColor = SystemColors.GradientActiveCaption;
+            button7.Font = new Font("Tahoma", 7.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button7.ForeColor = SystemColors.ActiveCaptionText;
+            button7.Location = new Point(1030, 1026);
+            button7.Name = "button7";
+            button7.Size = new Size(94, 29);
+            button7.TabIndex = 26;
+            button7.Text = "PRINT";
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += button7_Click;
+            // 
+            // button9
+            // 
+            button9.BackColor = SystemColors.ActiveCaptionText;
+            button9.Font = new Font("Tahoma", 7.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button9.ForeColor = SystemColors.ButtonHighlight;
+            button9.Location = new Point(12, 12);
+            button9.Name = "button9";
+            button9.Size = new Size(94, 29);
+            button9.TabIndex = 28;
+            button9.Text = "BACK";
+            button9.UseVisualStyleBackColor = false;
+            button9.Click += button9_Click;
+            // 
+            // vScrollBar1
+            // 
+            vScrollBar1.Location = new Point(1149, 39);
+            vScrollBar1.Name = "vScrollBar1";
+            vScrollBar1.Size = new Size(19, 1074);
+            vScrollBar1.TabIndex = 29;
+            vScrollBar1.Scroll += vScrollBar1_Scroll;
+            // 
+            // Gilles
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.GradientActiveCaption;
+            ClientSize = new Size(1162, 1055);
+            Controls.Add(richTextBox5);
+            Controls.Add(button9);
+            Controls.Add(button7);
+            Controls.Add(richTextBox6);
+            Controls.Add(label7);
+            Controls.Add(textBox5);
+            Controls.Add(label6);
+            Controls.Add(textBox4);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(richTextBox4);
+            Controls.Add(richTextBox3);
+            Controls.Add(richTextBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(pictureBox1);
+            Controls.Add(button1);
+            Controls.Add(vScrollBar1);
+            Controls.Add(button3);
+            Controls.Add(button4);
+            Controls.Add(button5);
+            Controls.Add(button6);
+            Font = new Font("Segoe UI", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ForeColor = SystemColors.ActiveCaption;
+            Margin = new Padding(0, 6, 0, 6);
+            Name = "Gilles";
+            Text = "Gilles";
+            Load += Gilles_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
-
         #endregion
     }
 }
--- WinFormsApp1/Gilles.cs
+++ WinFormsApp1/Gilles.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,211 @@ namespace WinFormsApp1
 {
     public partial class Gilles : Form
     {
+        // Add printing components
+        private PrintDocument printDocument;
+        private PrintPreviewDialog printPreviewDialog;
+
         public Gilles()
         {
             InitializeComponent();
+            
+            // Initialize the printing components
+            SetupPrinting();
+        }
+
+        private void SetupPrinting()
+        {
+            // Create the PrintDocument
+            printDocument = new PrintDocument();
+            printDocument.PrintPage += PrintDocument_PrintPage;
+            
+            // Create the PrintPreviewDialog
+            printPreviewDialog = new PrintPreviewDialog
+            {
+                Document = printDocument,
+                UseAntiAlias = true,
+                ShowIcon = false,
+                ClientSize = new Size(800, 600)
+            };
+        }
+
+        private void PrintDocument_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            try
+            {
+                // Layout variables
+                float marginLeft = 100;
+                float marginTop = 100;
+                float lineHeight = 30;
+                float currentY = marginTop;
+                Font headerFont = new Font("Arial", 16, FontStyle.Bold);
+                Font labelFont = new Font("Arial", 12, FontStyle.Bold);
+                Font valueFont = new Font("Arial", 12, FontStyle.Regular);
+
+                // Draw photo if available
+                if (pictureBox1.Image != null)
+                {
+                    Rectangle imageRect = new Rectangle((int)(marginLeft + 350), (int)marginTop, 120, 150);
+                    e.Graphics.DrawImage(pictureBox1.Image, imageRect);
+                }
+
+                // Draw header
+                e.Graphics.DrawString("CURRICULUM VITAE", headerFont, Brushes.Black, marginLeft, currentY);
+                currentY += lineHeight * 2;
+
+                // Draw Name
+                e.Graphics.DrawString("Name:", labelFont, Brushes.Black, marginLeft, currentY);
+                e.Graphics.DrawString(textBox2.Text, valueFont, Brushes.Black, marginLeft + 100, currentY);
+                currentY += lineHeight;
+
+                // Draw Status
+                e.Graphics.DrawString("Status:", labelFont, Brushes.Black, marginLeft, currentY);
+                e.Graphics.DrawString(textBox3.Text, valueFont, Brushes.Black, marginLeft + 100, currentY);
+                currentY += lineHeight;
+
+                // Draw Contact Info
+                e.Graphics.DrawString("Phone:", labelFont, Brushes.Black, marginLeft, currentY);
+                e.Graphics.DrawString(textBox1.Text, valueFont, Brushes.Black, marginLeft + 100, currentY);
+                currentY += lineHeight;
+
+                // Draw Email
+                e.Graphics.DrawString("Email:", labelFont, Brushes.Black, marginLeft, currentY);
+                e.Graphics.DrawString(textBox4.Text, valueFont, Brushes.Black, marginLeft + 100, currentY);
+                currentY += lineHeight;
+
+                // Draw Address
+                e.Graphics.DrawString("Address:", labelFont, Brushes.Black, marginLeft, currentY);
+                e.Graphics.DrawString(textBox5.Text, valueFont, Brushes.Black, marginLeft + 100, currentY);
+                currentY += lineHeight * 1.5f;
+
+                // Draw About Me section
+                e.Graphics.DrawString("ABOUT ME", labelFont, Brushes.Black, marginLeft, currentY);
+                currentY += lineHeight;
+                e.Graphics.DrawString(richTextBox2.Text, valueFont, Brushes.Black, marginLeft, currentY);
+                currentY += GetTextHeight(richTextBox2.Text, valueFont, e.Graphics, 600) + lineHeight;
+
+                // Draw Education section
+                e.Graphics.DrawString("EDUCATION", labelFont, Brushes.Black, marginLeft, currentY);
+                currentY += lineHeight;
+                e.Graphics.DrawString(richTextBox3.Text, valueFont, Brushes.Black, marginLeft, currentY);
+                currentY += GetTextHeight(richTextBox3.Text, valueFont, e.Graphics, 600) + lineHeight;
+
+                // Draw Experience section
+                e.Graphics.DrawString("EXPERIENCE", labelFont, Brushes.Black, marginLeft, currentY);
+                currentY += lineHeight;
+                e.Graphics.DrawString(richTextBox4.Text, valueFont, Brushes.Black, marginLeft, currentY);
+                currentY += GetTextHeight(richTextBox4.Text, valueFont, e.Graphics, 600) + lineHeight;
+
+                // Draw Skills section
+                e.Graphics.DrawString("SKILLS", labelFont, Brushes.Black, marginLeft, currentY);
+                currentY += lineHeight;
+                e.Graphics.DrawString(richTextBox6.Text, valueFont, Brushes.Black, marginLeft, currentY);
+                currentY += GetTextHeight(richTextBox6.Text, valueFont, e.Graphics, 600) + lineHeight;
+
+                // Draw References section
+                e.Graphics.DrawString("REFERENCES", labelFont, Brushes.Black, marginLeft, currentY);
+                currentY += lineHeight;
+                e.Graphics.DrawString(richTextBox5.Text, valueFont, Brushes.Black, marginLeft, currentY);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error printing: {ex.Message}", "Print Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Helper method to calculate text height
+        private float GetTextHeight(string text, Font font, Graphics graphics, int maxWidth)
+        {
+            if (string.IsNullOrEmpty(text))
+                return font.Height;
+
+            SizeF size = graphics.MeasureString(text, font, maxWidth);
+            return size.Height + 10; // Add some padding
+        }
+        
+        // Event handlers
+        private void button7_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Show print dialog
+                using (PrintDialog printDialog = new PrintDialog())
+                {
+                    printDialog.Document = printDocument;
+                    if (printDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        // Show print preview
+                        printPreviewDialog.ShowDialog();
+                        
+                        // Uncomment this and comment the above line if you want to print directly
+                        // printDocument.Print();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Print error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Existing event handlers
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Implementation for cellphone textbox
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Title = "Select a photo";
+                openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        pictureBox1.Image = Image.FromFile(openFileDialog.FileName);
+                        pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error loading image: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+        
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            // Implementation if needed
+        }
+
+        private void Gilles_Load(object sender, EventArgs e)
+        {
+            // Set up scrollbar
+            this.AutoScroll = true;
+        }
+        
+        private void label3_Click(object sender, EventArgs e)
+        {
+            // Implementation if needed
+        }
+        
+        private void label7_Click(object sender, EventArgs e)
+        {
+            // Implementation if needed
+        }
+        
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.Show();
+            this.Hide();
+        }
+        
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            this.AutoScroll = true;
         }
     }
 }
